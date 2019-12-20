f0ce2f7372b9dd302802f4dd2d407e1b43f846e6
miaklwalker <ouisihai@gmail.com>
2019-12-19 20:25:49 -0600

    commit

--- routes/Login.route.js
+++ routes/Login.route.js
@@ -6,27 +6,24 @@ const jwt = require('jsonwebtoken');
 
 
 router.post('/', async (req,res)=>{
-    console.log(req.body);
+
     // Checking for incorrect entries
     const {error} =  loginValidation(req.body);
     if(error)return res.status(400).send(error)
-    console.log('Made it past validation')
+
     //Checking if user is unique
     const user = await User.findOne({email:req.body.email});
     if(!user)return res.status(400).send('Email or Password is incorrect');
-    console.log('made it past log in validation')
-    console.log(user)
-    
     //Checking Password
     const validPassword = await bcryptjs.compare(req.body.password,user.password)
     if(!validPassword)return res.status(400).send('Email or Password is incorrect');
-    console.log('you made it past log in validation again')
+
     //create and assign token
     const secret = process.env.TOKEN_SECRET
     const token = jwt.sign({_id:user._id},secret);
-    console.log('token made')
+
     res.header('auth-token',token).send(token)
-    console.log('token sent')
+
 
 })
 
